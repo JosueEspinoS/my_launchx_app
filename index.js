207987152dfac26c809e03e2d0f7c3dab26973d9
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-21 18:19:15 -0500

    creacion archivo index.js con instancia de 3 objetos

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander.js');
+const josue = new MissionCommander("Josue");
+const diana = new MissionCommander("Diana")
+const miguel = new MissionCommander("Miguel")
+console.log(josue);
+console.log(diana);
+console.log(miguel);
